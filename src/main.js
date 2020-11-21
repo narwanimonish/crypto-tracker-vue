bb731574b5d058db88435fc7d00d8409c8518da3
Monish Narwani <monish.narwani@focalworks.in>
2020-11-21 18:39:54 +0530

    Added Vuex and fetch currencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import App from './App.vue';
+import Store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+  store: Store,
+}).$mount('#app');
---
+++ src/store.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    count: 0,
+    currencies: [],
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    setCurrenciesList(state, currencies) {
+      state.currencies = currencies;
+    },
+  },
+  actions: {
+    getListOfTopCrypoCurrencies(context) {
+      Vue.$http
+        .get('https://api.coinranking.com/v1/public/coins/?limit=100')
+        .then((response) => {
+          if (response.status == 200) {
+            context.commit('setCurrenciesList', response.data.data);
+          }
+        });
+    },
+  },
+});
